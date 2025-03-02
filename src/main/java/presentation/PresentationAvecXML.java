52f3a41d0174bc7835026005112e122b70094bea
ElhoucineElmourabit <elhoucine.elmourabit.pro@gmail.com>
2025-03-02 16:26:54 +0000

    Injection des dépendances avec Spring - Version XML

---
+++ src/main/java/presentation/PresentationAvecXML.java
@@ -0,0 +1,16 @@
+package presentation;
+
+import metier.IMetier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PresentationAvecXML {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        //IMetier metier = (IMetier) context.getBean("metier");
+        IMetier metier = context.getBean(IMetier.class);
+        System.out.println("Res = "+metier.calcul());
+
+    }
+}
